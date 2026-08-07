722ca1cbad333b7b95a3be78622e4e1d6c0a1dcb
jgwdvv96pf-ops <jgwdvv96pf@privaterelay.appleid.com>
2026-08-07 02:16:11 +0100

    Delete app directory

--- app/api/logout/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { AUTH_COOKIE } from "@/lib/auth";
-
-export async function POST() {
-  const res = NextResponse.json({ ok: true });
-  res.cookies.delete(AUTH_COOKIE);
-  return res;
-}
